58f2817ca2bdeb474e33859bcc19f7fac7ad54f9
Nanthakumar <nnantha971@gmail.com>
2026-01-30 17:25:51 +0530

    the user and the color game was routed and  the MUI packages was installed via terminal

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
+import ColorGame from './ColorGame';
 import MovieDetails from './MovieDetails';
+import UserDetails from './UserDetails';
 import { Route, Routes, Link } from 'react-router';
 
 
@@ -17,11 +19,18 @@ function App() {
 
 
 <nav>
-  <Link to="/movies">Movies</Link>
+<ul>
+  <li> <Link to="/movies">Movies</Link></li>
+  <li><Link to="/colorgame">Color Game</Link></li>
+  <li><Link to="/user">User</Link></li>
+</ul>
 </nav>
 
 <Routes>
   <Route path="movies"element={<MovieDetails/>}/>
+  <Route path="colorgame"element={<ColorGame/>}/>
+  <Route path="user"element={<UserDetails/>}/>
+
 </Routes>
 
   </>
